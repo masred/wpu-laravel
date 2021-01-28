52313e28cad88a50383f25df5f1c1be800bcdf9c
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-28 23:50:18 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+
+@section('title', 'Web Programming UNPAS')
+
+@section('container')
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-10">
+            <h1>Hello, world!</h1>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="{{url('/')}}">Home</a>
+                    <a class="nav-link" href="{{url('/about')}}">About</a>
+                    <a class="nav-link" href="{{url('/mahasiswa')}}">Mahasiswa</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    @yield('container')
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+
+</html>
