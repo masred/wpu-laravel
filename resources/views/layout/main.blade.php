8d6a032bab80faced56f8ae9a4d1dbed9006e46d
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-29 18:20:13 +0700

    changed from mahasiswa to students

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -23,7 +23,7 @@
                 <div class="navbar-nav">
                     <a class="nav-link active" aria-current="page" href="{{url('/')}}">Home</a>
                     <a class="nav-link" href="{{url('/about')}}">About</a>
-                    <a class="nav-link" href="{{url('/mahasiswa')}}">Mahasiswa</a>
+                    <a class="nav-link" href="{{url('/students')}}">Students</a>
                 </div>
             </div>
         </div>
