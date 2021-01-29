5c8e04b0a4710163cc8947cf44c38787868401b6
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-29 23:50:32 +0700

    added resource routes

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,12 @@ use App\Http\Controllers\StudentsController;
 Route::get('/', [PagesController::class, 'home']);
 Route::get('/about', [PagesController::class, 'about']);
 
-Route::get('/students', [StudentsController::class, 'index']);
-Route::get('/students/create', [StudentsController::class, 'create']);
-Route::get('/students/{student}', [StudentsController::class, 'show']);
-Route::post('/students', [StudentsController::class, 'store']);
+// Route::get('/students', [StudentsController::class, 'index']);
+// Route::get('/students/create', [StudentsController::class, 'create']);
+// Route::get('/students/{student}', [StudentsController::class, 'show']);
+// Route::post('/students', [StudentsController::class, 'store']);
+// Route::delete('/students/{student}', [StudentsController::class, 'destroy']);
+// Route::get('/students/{student}/edit', [StudentsController::class, 'edit']);
+// Route::patch('/students/{student}', [StudentsController::class, 'update']);
+
+Route::resource('students', 'App\Http\Controllers\StudentsController');
