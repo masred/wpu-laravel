0819e41e4862a0ddf016ed84970f2df6cb5699e3
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-29 23:49:34 +0700

    old value

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -11,28 +11,28 @@
               @csrf
               <div class="mb-3">
                 <label for="nama" class="form-label">Name</label>
-                <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" placeholder="Enter name">
+                <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" placeholder="Enter name" value="{{ old('nama') }}">
                 @error('nama')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
               </div>
               <div class="mb-3">
                 <label for="nrp" class="form-label">NRP</label>
-                <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" name="nrp" placeholder="Enter NRP">
+                <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" name="nrp" placeholder="Enter NRP" value="{{ old('nrp') }}">
                 @error('nrp')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
               </div>
               <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Enter email">
+                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Enter email" value="{{ old('email') }}">
                 @error('email')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
               </div>
               <div class="mb-3">
                 <label for="jurusan" class="form-label">Jurusan</label>
-                <input type="text" class="form-control @error('jurusan') is-invalid @enderror" id="jurusan" name="jurusan" placeholder="Enter jurusan">
+                <input type="text" class="form-control @error('jurusan') is-invalid @enderror" id="jurusan" name="jurusan" placeholder="Enter jurusan" value="{{ old('jurusan') }}">
                 @error('jurusan')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
