f20998d012dd0444ef0ac0189938db2a0efa6e0d
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-29 23:50:17 +0700

    button delete and edit

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -11,9 +11,14 @@
                 <div class="card-body">
                   <h5 class="card-title">{{ $student->nama }}</h5>
                   <h6 class="card-subtitle mb-2 text-muted">{{ $student->nrp }}</h6>
+                  <p class="card-text">{{ $student->email }}</p>
                   <p class="card-text">{{ $student->jurusan }}</p>
-                  <button type="submit" class="btn btn-sm btn-primary">edit</button>
-                  <button type="submit" class="btn btn-sm btn-danger">delete</button>
+                  <a href="{{ $student->id }}/edit" class="btn btn-primary">edit</a>
+                  <form action="/students/{{ $student->id }}" method="POST" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">delete</button>
+                  </form>
                   <a href="/students" class="card-link">kembali</a>
                 </div>
               </div>
