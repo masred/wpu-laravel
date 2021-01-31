e62ed9fe50dbf97ded3fd242883d86f84ee7feff
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-31 19:01:14 +0700

    fix url detail

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -17,7 +17,7 @@
                 @foreach ($students as $student)
                     <li class="list-group-item d-flex justify-content-between align-items-center">
                         {{ $student->nama }}
-                        <a href="/students/{{ $student->id }}"><span  class="badge bg-info">detail</span></a>
+                        <a href="{{ url("/students/{$student->id}") }}"><span  class="badge bg-info">detail</span></a>
                     </li>
                 @endforeach
               </ul>
